f0868bd3a54db701feb6c28b38d61a1a983c9aea
Scott Sutton <scosut@hotmail.com>
2021-08-04 10:13:47 -0700

    Added fade in animation to every page.

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,34 @@
-(function() {
-	function close(nav, el, li, e) {
-		if (e != undefined) {
-			e.preventDefault();
-		}
+(function () {
+  function close(nav, el, li, e) {
+    if (e != undefined) {
+      e.preventDefault();
+      nav.style.visibility = "visible";
+    }
 
-		var isHidden  = el.className.indexOf("hide") >= 0;
-		var cls       = isHidden ? "fadeOut" : "fadeIn";
-		el.className  = "fas fa-chevron-circle-down";
-		el.className += isHidden ? " show" : " hide";
-		nav.className = isHidden ? "dissolve" : "";
-		
-		for (var i=1; i<li.length; i++) {
-			li[i].className = cls;
-		}
-	}
-	
-	function events() {
-		var nav = document.getElementById("nav");
-		var el  = document.getElementById("menu");
-		var li  = nav.querySelectorAll("ul li");
-		el.addEventListener("click", close.bind(this, nav, el, li));
-		
-		close(nav, el, li);
-	}
+    var isHidden = el.className.indexOf("hide") >= 0;
+    var cls = isHidden ? "fadeOut" : "fadeIn";
+    el.className = "fas fa-chevron-circle-down";
+    el.className += isHidden ? " show" : " hide";
+    nav.className = isHidden ? "dissolve" : "";
 
-	events();
+    for (var i = 1; i < li.length; i++) {
+      li[i].className = cls;
+    }
+  }
+
+  function events() {
+    var nav = document.getElementById("nav");
+    var pg = nav.getAttribute("page");
+    var el = document.getElementById("menu");
+    var li = nav.querySelectorAll("ul li");
+    el.addEventListener("click", close.bind(this, nav, el, li));
+
+    if (pg == 'resume') {
+      nav.style.visibility = "hidden";
+    }
+
+    close(nav, el, li);
+  }
+
+  events();
 })();
